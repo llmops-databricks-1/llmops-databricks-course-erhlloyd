76ab9a35ec83ef54de9b8efcd0e152fd499874a9
erhlloyd <e.rh.lloyd@gmail.com>
2026-04-07 21:48:52 +0200

    update config and add outputs

--- src/mushroom_data_preprocessing/config.py
+++ src/mushroom_data_preprocessing/config.py
@@ -3,8 +3,7 @@ import os
 from dynaconf import Dynaconf
 from loguru import logger
 
-pth = os.path.dirname(__file__)
-settings_files_yml = os.path.join(pth, "../../project_config.yml")
+settings_files_yml = "../project_config.yml"
 logger.debug(f"Settings_files set to: {settings_files_yml}")
 
 settings = Dynaconf(
@@ -12,3 +11,7 @@ settings = Dynaconf(
     settings_files=[settings_files_yml],
     environments=False,
 )
+logger.debug(f"settings configured files: {settings._loaded_files}")
+
+if settings._loaded_files:
+    logger.debug(f"{os.path.exists(settings._loaded_files[0])}")
